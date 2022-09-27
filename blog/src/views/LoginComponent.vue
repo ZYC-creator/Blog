e84504f9627d21945ed8dad9f9caf0e2ef0f1db8
540604007@qq.com <540604007@qq.com>
2022-09-27 17:30:48 +0800

    create vue project and express project

---
+++ blog/src/views/LoginComponent.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+ 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios'
+    export default defineComponent({
+        name:'LoginComponent',
+        setup(){
+            let msg ='';
+            axios.get("http://localhost:8080/login")
+            .then((res) => {
+                msg = res.data;
+                console.log(msg);
+                
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+            return {
+                msg
+            }
+    },
+    })
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+</style>
